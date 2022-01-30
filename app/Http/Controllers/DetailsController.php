8bf20c7a1507da7142d303850e6ebac9aa5598ab
Abraham Peter <theclassical001@gmail.com>
2022-01-30 18:00:31 +0100

    crud + search done

---
+++ app/Http/Controllers/DetailsController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Detail;
+
+class DetailsController extends Controller
+{
+    public function getAllDetails(Request $request){
+        $details = Detail::all();
+        if($details != null) {
+            return $details;
+        }else{
+            return ["no details"];
+        }
+    }
+
+    public function createDetail(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'state' => 'required',
+            'address' => 'required',
+        ]);
+
+          return Detail::create($request->all());
+    }
+
+    public function getDetail($id){
+        $d = Detail::find($id);
+
+        return $d;
+    }
+
+    public function updateDetail(Request $request, $id) {
+        $d = Detail::find($id);
+        $d->update($request->all());
+        return $d;
+    }
+
+    public function searchDetail($name){
+        $d = Detail::where('name', 'like', '%'.$name.'%')->get();
+        return $d;
+    }
+
+    public function deleteDetail($id) {
+        $d = Detail::find($id);
+        $d->delete($id);
+        return [
+            
+            'success' => $d,
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\DetailsController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/details',[App\Http\Controllers\DetailsController::class, 'getAllDetails']);
+Route::post('/details',[App\Http\Controllers\DetailsController::class, 'createDetail']);
+Route::get('/details/{id}',[App\Http\Controllers\DetailsController::class, 'getDetail']);
+Route::get('/details/search/{name}',[App\Http\Controllers\DetailsController::class, 'searchDetail']);
+Route::get('/details/update/{id}',[App\Http\Controllers\DetailsController::class, 'updateDetail']);
+Route::delete('/details/delete/{id}',[App\Http\Controllers\DetailsController::class, 'deleteDetail']);
