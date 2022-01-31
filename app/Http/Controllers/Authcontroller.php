9da19240cc1b7711beaef6ba884c2251373f8568
Abraham Peter <theclassical001@gmail.com>
2022-01-31 10:33:57 +0100

    api updated + register & logout in authcontroller

--- app/Http/Controllers/Authcontroller.php
+++ app/Http/Controllers/Authcontroller.php
@@ -9,5 +9,35 @@ use Illuminate\Support\Facades\Hash;
 
 class Authcontroller extends Controller
 {
-    //
+    public function register(Request $request){
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request){
+
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'logged out'
+        ];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DetailsController;
+use App\Http\Controllers\Authcontroller;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,7 @@ use App\Http\Controllers\DetailsController;
 |
 */
 
+Route::post('/register',[App\Http\Controllers\Authcontroller::class, 'register']);
 
 Route::get('/details',[App\Http\Controllers\DetailsController::class, 'getAllDetails']);
 Route::get('/details/{id}',[App\Http\Controllers\DetailsController::class, 'getDetail']);
@@ -21,10 +23,12 @@ Route::get('/details/search/{name}',[App\Http\Controllers\DetailsController::cla
 
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    
+
     Route::post('/details',[App\Http\Controllers\DetailsController::class, 'createDetail']);
     Route::put('/details/update/{id}',[App\Http\Controllers\DetailsController::class, 'updateDetail']);
     Route::delete('/details/delete/{id}',[App\Http\Controllers\DetailsController::class, 'deleteDetail']);
     Route::delete('/details/deleteall',[App\Http\Controllers\DetailsController::class, 'deleteAll']);
+    Route::post('/logout',[App\Http\Controllers\Authcontroller::class, 'logout']);
+
     
 });
